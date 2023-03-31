3b85ca130b64d165942f28f57b42492cbce6bef5
Daniel Mundra <daniel.mundra@civicactions.com>
2023-03-31 15:34:34 -0700

    Added tests for the new template parameter.

--- tests/openacr-output-cli.test.ts
+++ tests/openacr-output-cli.test.ts
@@ -240,4 +240,86 @@ describe("OpenACR CLI test output", () => {
       );
     });
   });
+
+  it("when passed markdown template file option should return valid output message", () => {
+    const valid = spawn(
+      cmd,
+      options.concat(
+        "tests/examples/valid.yaml",
+        "-c",
+        "catalog/2.4-edition-wcag-2.0-508-en.yaml",
+        "-o",
+        "output/valid.markdown",
+        "-t",
+        "templates/openacr-markdown-0.1.0.handlebars"
+      )
+    );
+    const chunks = [];
+
+    valid.stdout.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    valid.stdout.on("end", () => {
+      const output = Buffer.concat(chunks).toString();
+
+      expect(output).to.equal(
+        "Valid and output generated at output/valid.markdown!\n"
+      );
+    });
+  });
+
+  it("when passed html template file option should return valid output message", () => {
+    const valid = spawn(
+      cmd,
+      options.concat(
+        "tests/examples/valid.yaml",
+        "-c",
+        "catalog/2.4-edition-wcag-2.0-508-en.yaml",
+        "-o",
+        "output/valid.html",
+        "-t",
+        "templates/openacr-simple-html-0.1.0.handlebars"
+      )
+    );
+    const chunks = [];
+
+    valid.stdout.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    valid.stdout.on("end", () => {
+      const output = Buffer.concat(chunks).toString();
+
+      expect(output).to.equal(
+        "Valid and output generated at output/valid.html!\n"
+      );
+    });
+  });
+
+  it("when passed invalid template file option should return invalid template output message", () => {
+    const invalid = spawn(
+      cmd,
+      options.concat(
+        "tests/examples/valid.yaml",
+        "-c",
+        "catalog/2.4-edition-wcag-2.0-508-en.yaml",
+        "-o",
+        "output/valid.html",
+        "-t",
+        "templates/non-existent-template.handlerbars"
+      )
+    );
+    const chunks = [];
+
+    invalid.stderr.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    invalid.stderr.on("end", () => {
+      const output = Buffer.concat(chunks).toString();
+
+      expect(output).to.equal("Invalid: template file is invalid.\n");
+    });
+  });
 });
